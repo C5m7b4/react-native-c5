0eef2f048bcaa66add56c53a0e721e35b0fa1356
C5m7b4 <mbedingfield@dcrpos.com>
2023-09-20 15:21:00 -0500

    add time travel for testing

--- src/__tests__/BarChart.test.tsx
+++ src/__tests__/BarChart.test.tsx
@@ -3,11 +3,30 @@ import { BarChart } from '../charts';
 import renderer from 'react-test-renderer';
 import { testData } from '../testData/data';
 
+jest.mock('react-native', () => {
+  const rn = jest.requireActual('react-native');
+  const spy = jest.spyOn(rn.Animated, 'createAnimatedComponent');
+  spy.mockImplementation(() => jest.fn(() => null));
+  return rn;
+});
+
 describe('BarChart', () => {
   test('should render properly', () => {
-    const tree = renderer
-      .create(<BarChart data={testData} x_key="month" y_key="value" />)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+    // @ts-ignore
+    global.withAnimatedTimeTravelEnabled(() => {
+      const tree = renderer
+        .create(
+          <BarChart
+            animated={false}
+            data={testData}
+            x_key="month"
+            y_key="value"
+          />
+        )
+        .toJSON();
+      // @ts-ignore
+      global.timeTravel(2000);
+      expect(tree).toMatchSnapshot();
+    });
   });
 });
