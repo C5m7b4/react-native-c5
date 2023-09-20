6bc5a8009fe08382d932e71eaac74008d8de7c5d
C5m7b4 <mbedingfield@dcrpos.com>
2023-09-20 13:16:26 -0500

    add tests for linechart, barchart, and boxplot

--- src/__tests__/newShade.test.ts
+++ src/__tests__/newShade.test.ts
@@ -3,5 +3,8 @@ import { newShade } from '../utils';
 describe('newShade', () => {
   test('should return 0000ff', () => {
     expect(newShade('#ff0000', 50)).toEqual('#ff32ff');
+    expect(newShade('#00ff00', 50)).toEqual('#32ffff');
+    expect(newShade('#fff', 50)).toEqual('fff');
+    expect(newShade('#00ffff', 50)).toEqual('#32ffff');
   });
 });
